8b3b1cdcd5e8aabbb5ef8aee5828cbfb4ef13cb1
lunoxximpostor <120766692+lunoxximpostor@users.noreply.github.com>
2023-03-06 00:28:35 +0700

    ngent0t lu

--- ssh/portovpn.sh
+++ ssh/portovpn.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/annelyah23/IP/main/access | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/lunoxximpostor/4344mSDFNANSKJBNHGBRASsadsahbPX/main/access | grep $MYIP )
 if [ $MYIP = $MYIP ]; then
 echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
